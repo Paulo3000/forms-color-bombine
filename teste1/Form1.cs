d8f40b6a0b67ac29b0c9ce4ed27ef1bf5c9c165f
Paulo3000 <1988paulosergio@gmail.com>
2021-04-13 12:25:12 -0300

    first comit

---
+++ teste1/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace teste1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = colorDialog1.ShowDialog();
+            if(result == DialogResult.OK)
+            {
+                panel1.BackColor = colorDialog1.Color;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult result = colorDialog1.ShowDialog();
+            if(result == DialogResult.OK)
+            {
+                panel2.BackColor = colorDialog1.Color;
+            }
+        }
+    }
+}
